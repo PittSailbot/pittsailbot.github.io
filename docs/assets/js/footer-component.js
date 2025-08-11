209c53a17f2abe9fca8d7486f479f72b50bc9a3f
aaron4522 <rex4522@gmail.com>
2025-08-11 17:47:19 -0400

    Fix fa-icon rendering

--- docs/assets/js/footer-component.js
+++ docs/assets/js/footer-component.js
@@ -12,7 +12,6 @@ class SailbotFooter extends HTMLElement {
                         <section>
                             <h3>Follow us!</h3>
                             <ul class="plain">
-                                <script src="https://kit.fontawesome.com/dbf179c885.js" crossorigin="anonymous"></script>
                                 <li><a href="https://www.instagram.com/sailbot_pitt/"><i class="fa-brands fa-instagram" style="color: #ffffff;">
                                 &nbsp;</i>Instagram</a></li>
                                 <li><a href="https://github.com/PittSailbot/SailBot"><i class="fa-brands fa-github" style="color: #ffffff;">
